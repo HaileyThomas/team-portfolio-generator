db6f83693005494bebed5aef4a0a09049c7247db
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-15 16:07:54 -0700

    create first test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,9 @@
 const Employee = require("../lib/Employee.js");
 
 test("creates an employee object", () => {
-    const employee = new Employee("Matt");
+    const employee = new Employee("Wendy", "01", "wendy@email.com");
 
-    expect(employee.name).toBe("Matt");
+    expect(employee.name).toBe("Wendy");
+    expect(employee.id).toBe("01");
+    expect(employee.email).toBe("wendy@email.com");
 });
-
