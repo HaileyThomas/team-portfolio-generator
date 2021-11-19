2dd4a1d681d2b181f371425d13a0e79826460725
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-19 15:59:33 -0700

    finish write file function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const { writeFile } = require("./utils/generate");
 let employees = [];
 
 function initializeApp() {
---
+++ utils/page-template.js
@@ -0,0 +1,31 @@
+
+
+
+module.exports = templateData => {
+    // destructure data (review portfolio generator project)
+    // const { projects, about, ...header } = templateData;
+
+    // return html string
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie-edge">
+            <title>Our Work Team</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+            <link rel="stylesheet" href="../dist/style.css">
+        </head>
+        <body>
+            <header>
+            
+            </header>
+            <main>
+            </main>
+            <footer>
+            </footer>
+        </body>
+        </html>
+    `;
+};
